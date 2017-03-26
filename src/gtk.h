8beac64cddd2307b9deff10fd0e7caaa58ce2b01
Adrien Jeser <adrien@jeser.me>
2017-03-26 15:52:03 +0200

    Cmake (Close #2)

---
+++ src/gtk.h
@@ -0,0 +1,10 @@
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+#pragma clang diagnostic ignored "-Wreserved-id-macro"
+#pragma clang diagnostic ignored "-Wdocumentation"
+#pragma clang diagnostic ignored "-Wdocumentation-unknown-command"
+#pragma clang diagnostic ignored "-Wpadded"
+
+#include <gtk/gtk.h>
+
+#pragma GCC diagnostic pop
---
+++ src/main_test.c
@@ -0,0 +1,24 @@
+#include <setjmp.h>
+#include <stdarg.h>
+#include <stddef.h>
+
+#include <cmocka.h>
+
+static int setup(void **state) {
+    (void)state;
+    return 0;
+}
+
+static int teardown(void **state) {
+    (void)state;
+    return 0;
+}
+
+static void null_test(void **state) { (void)state; }
+
+int main(void) {
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test_setup_teardown(null_test, setup, teardown),
+    };
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
