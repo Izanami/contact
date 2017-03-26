3f30faa92bce3dbcaa97c4bef734f79be5fafb02
Adrien Jeser <adrien@jeser.me>
2017-03-26 20:24:08 +0200

    Display store csv (#3)

---
+++ src/tree.c
@@ -0,0 +1,33 @@
+#include "gtk.h"
+
+#include "tree.h"
+
+#pragma GCC diagnostic push
+#pragma clang diagnostic ignored "-Wdisabled-macro-expansion"
+
+/// \brief Window class
+struct _ContactTree {
+    GtkListStore parent;  ///< Parent
+};
+
+G_DEFINE_TYPE(ContactTree, contact_tree, GTK_TYPE_LIST_STORE)
+
+static void contact_tree_init(ContactTree *tree) {
+    GtkListStore *list = GTK_LIST_STORE(tree);
+    GType column[] = {G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING,
+                      G_TYPE_STRING, G_TYPE_STRING};
+
+    gtk_list_store_set_column_types(list, N_COLUMNS, column);
+
+    GtkTreeIter iter;
+    gtk_list_store_append(list, &iter);
+    gtk_list_store_set(list, &iter, COLUMN_FIRSTNAME, "Adrien", -1);
+}
+
+static void contact_tree_class_init(ContactTreeClass *class) { (void)class; }
+
+ContactTree *contact_tree_new() {
+    return g_object_new(CONTACT_TREE_TYPE, NULL, NULL, NULL);
+}
+
+#pragma GCC diagnostic pop
---
+++ src/tree.h
@@ -0,0 +1,20 @@
+#ifndef CONTACT_TREE_H_
+#define CONTACT_TREE_H_
+
+#include "gtk.h"
+
+#define CONTACT_TREE_TYPE (contact_tree_get_type())
+G_DECLARE_FINAL_TYPE(ContactTree, contact_tree, CONTACT, TREE, GtkListStore)
+
+ContactTree *contact_tree_new(void);
+
+enum {
+    COLUMN_FIRSTNAME,
+    COLUMN_LASTNAME,
+    COLUMN_MAIL,
+    COLUMN_PHONE,
+    COLUMN_BIRTH,
+    N_COLUMNS
+};
+
+#endif /* CONTACT_TREE_H_ */
